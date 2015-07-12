c0bee8ad40b78c86e92e1b11c727e6451bfe97b6
pjanczyk <pjanczyk97@gmail.com>
2015-07-12 16:19:07 +0200

    .

--- .openshift/cron/minutely/cron.sh
+++ .openshift/cron/minutely/cron.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-date >> ${OPENSHIFT_PHP_LOG_DIR}/cron.log
+export TZ = "/usr/share/zoneinfo/Europe/Warsaw"
+NOW = $(date +%F)
+FILE = ${OPENSHIFT_LOG_DIR}/cron.log
+echo "[$NOW]" >> $FILE
+
 cd ${OPENSHIFT_REPO_DIR}
-php ${OPENSHIFT_REPO_DIR}/cron.php
+php ./cron.php >> $FILE
