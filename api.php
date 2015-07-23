bb20682e14bd477d6c016db29257bd1717988e20
pjanczyk <pjanczyk97@gmail.com>
2015-07-23 09:34:01 +0200

    `

--- api.php
+++ api.php
@@ -47,7 +47,7 @@ if ($args[0] == 'news' && count($args) == 3) { # /api/news/<class>/<lastModified
     $model = new NewsModel($db);
 
     $class = urldecode($args[1]);
-    $lastModified = date('Y-m-d H:i:s', $args[2]);
+    $lastModified = date('Y-m-d H:i:s', intval($args[2]));
     $now = time();
     $news = $model->get($class, date('Y-m-d H:i:s', $now), $lastModified);
 
