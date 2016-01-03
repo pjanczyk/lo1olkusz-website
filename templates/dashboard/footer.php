40c5ff5ae7d17b0fdf092526df3f8a75b5cd4dbb
Piotr Janczyk <pjanczyk97@gmail.com>
2016-01-03 22:46:32 +0100

    `

--- templates/dashboard/footer.php
+++ templates/dashboard/footer.php
@@ -3,7 +3,7 @@
 
 <footer class="footer">
     <div class="container <?= isset($wideContainer) ? '': 'container-narrow' ?>">
-        <p class="text-muted">&copy; 2015 Piotr Janczyk</p>
+        <p class="text-muted">&copy; 2016 Piotr Janczyk</p>
     </div>
 </footer>
 
