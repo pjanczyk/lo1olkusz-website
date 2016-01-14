a5018644e85e0c47ededa280500965a55836481a
Piotr Janczyk <pjanczyk97@gmail.com>
2016-01-14 21:11:13 +0100

    `

--- autoloader.php
+++ autoloader.php
@@ -2,9 +2,17 @@
 
 spl_autoload_register(function ($class) {
 
+    $prefix = 'pjanczyk\\lo1olkusz\\';
     $baseDir = __DIR__ . '/src/';
-    $file = $baseDir . str_replace('\\', '/', $class) . '.php';
 
+    $len = strlen($prefix);
+    if (strncmp($prefix, $class, $len) !== 0) {
+        return;
+    }
+
+    $relativeClass = substr($class, $len);
+
+    $file = $baseDir . str_replace('\\', '/', $relativeClass) . '.php';
     if (file_exists($file)) {
         /** @noinspection PhpIncludeInspection */
         require $file;
