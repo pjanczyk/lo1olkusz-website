10896a80d5807911b6133f53396a4d40fd5c81c9
Piotr Janczyk <pjanczyk97@gmail.com>
2017-09-27 19:54:29 +0200

    Add Docker configuration

--- app/code/config.php
+++ app/code/config.php
@@ -1,12 +1,12 @@
 <?php
 
 return [
-    'data_dir' => $_ENV['OPENSHIFT_DATA_DIR'],
-    'log_dir' => $_ENV['OPENSHIFT_PHP_LOG_DIR'],
-    'db_host' => $_ENV['OPENSHIFT_MYSQL_DB_HOST'],
-    'db_port' => $_ENV['OPENSHIFT_MYSQL_DB_PORT'],
-    'db_database' => 'lo1olkusz',
-    'db_user' => $_ENV['OPENSHIFT_MYSQL_DB_USERNAME'],
-    'db_password' => $_ENV['OPENSHIFT_MYSQL_DB_PASSWORD'],
+    'data_dir' => '/tmp',
+    'log_dir' => '/var/log',
+    'db_host' => getenv('DB_HOST'),
+    'db_port' => getenv('DB_PORT'),
+    'db_database' => getenv('DB_DATABASE'),
+    'db_user' => getenv('DB_USER'),
+    'db_password' => getenv('DB_PASSWORD'),
     'db_options' => [\PDO::MYSQL_ATTR_INIT_COMMAND => "SET time_zone = 'Europe/Warsaw'"]
 ];
