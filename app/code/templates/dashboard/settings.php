171408f649a2af7207f64f5e5d10c865d50e21de
Piotr Janczyk <pjanczyk97@gmail.com>
2017-09-27 20:39:54 +0200

    Fix settings page

--- app/code/templates/dashboard/settings.php
+++ app/code/templates/dashboard/settings.php
@@ -8,38 +8,6 @@
 <?php include 'templates/dashboard/alerts.php' ?>
 
 <div class="row">
-
-    <div class="col-sm-6">
-        <div class="pnl">
-            <form action="/dashboard/settings" method="post" enctype="multipart/form-data">
-                <div class="pnl-header">
-                    Autoaktualizacje aplikacji
-                    <button type="submit" class="btn btn-success btn-sm pull-right">Zapisz</button>
-                </div>
-                <div class="panel-body">
-                    <div class="form-group">
-                        <label for="apk-version">Wersja</label>
-                        <input type="text" class="form-control" name="version" id="version"
-                               placeholder="Wersja" value="<?= $version ?>">
-                    </div>
-                    <div class="form-group">
-                        <label for="apk-file">Plik APK</label>
-                        <?php if (isset($apkLastModified)): ?>
-                            <div class="panel panel-default">
-                                <div class="panel-body">
-                                    <a href="/download"><span class="last-modified"><?=formatTimestamp($apkLastModified)?></span></a>
-                                    <br>
-                                    <small>MD5: <?=$apkMd5?></small>
-                                </div>
-                            </div>
-                        <?php endif ?>
-                        <input type="file" name="apk" id="apk">
-                    </div>
-                </div>
-            </form>
-        </div>
-    </div>
-
     <div class="col-sm-6">
         <div class="pnl">
             <div class="pnl-header">
